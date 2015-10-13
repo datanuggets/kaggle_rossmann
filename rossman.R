002af4a6066db827c37fb7657f56290a749c024b
Jim Bijwaard <jimbijwaard@gmail.com>
2015-10-13 12:46:38 +0200

    Added R Script to predict sales using an RandomForest model. Code is fully based on https://www.kaggle.com/michaelpawlus/rossmann-store-sales/obligatory-xgboost-example/code

---
+++ rossman.R
@@ -0,0 +1,60 @@
+# Based on Ben Hamner script from Springleaf
+# https://www.kaggle.com/benhamner/springleaf-marketing-response/random-forest-example
+
+library(readr)
+library(randomForest)
+
+set.seed(123)
+
+train <- read_csv("data/train.csv")
+test  <- read_csv("data/test.csv")
+store <- read_csv("data/store.csv")
+
+train <- merge(train,store)
+test <- merge(test,store)
+
+# There are some NAs in the integer columns so conversion to zero
+train[is.na(train)]   <- 0
+test[is.na(test)]   <- 0
+
+# looking at only stores that were open in the train set
+train <- train[ which(train$Open=='1'),]
+
+# seperating out the elements of the date column for the train set
+train$month <- as.integer(format(train$Date, "%m"))
+train$year <- as.integer(format(train$Date, "%y"))
+train$day <- as.integer(format(train$Date, "%d"))
+
+# removing the date column from train (since elements are extracted) and also StateHoliday which has a lot of NAs
+train <- train[,-c(3,8)]
+train$StoreType.Assortment=paste(train$StoreType,train$Assortment)
+
+# seperating out the elements of the date column for the test set
+test$month <- as.integer(format(test$Date, "%m"))
+test$year <- as.integer(format(test$Date, "%y"))
+test$day <- as.integer(format(test$Date, "%d"))
+
+# removing the date column from test (since elements are extracted) and also StateHoliday which has a lot of NAs
+test <- test[,-c(4,7)]
+test$StoreType.Assortment=paste(test$StoreType,test$Assortment)
+
+feature.names <- names(train)[c(1,2,5:20)]
+
+for (f in feature.names) {
+  if (class(train[[f]])=="character") {
+    levels <- unique(c(train[[f]], test[[f]]))
+    train[[f]] <- as.integer(factor(train[[f]], levels=levels))
+    test[[f]]  <- as.integer(factor(test[[f]],  levels=levels))
+  }
+}
+
+clf <- randomForest(train[,feature.names],
+                    log(train$Sales+1),
+                    ntree=17,
+                    sampsize=200000,
+                    do.trace=TRUE)
+
+pred <- exp(predict(clf, test)) -1
+submission <- data.frame(Id=test$Id, Sales=pred)
+
+write_csv(submission, "rf1.csv")
